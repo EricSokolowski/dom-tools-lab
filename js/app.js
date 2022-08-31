b4f5c0db04d70709c19585f916a12f46bd28e28a
EricSoklowski <ericsoko2002@gmail.com>
2022-08-31 10:58:28 -0400

    remove unnecessary code

--- js/app.js
+++ js/app.js
@@ -5,8 +5,8 @@ console.log(input)
 console.log(btn)
 console.log(ul)
 
+
 btn.addEventListener('click', function(evt){
-  if(input.value === '') return
   const li = document.createElement('li')
   li.textContent = input.value
   ul.appendChild(li)
